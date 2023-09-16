fb264021e183a4a784278dd5ca6e3b9dd27c655f
kirtan <kirtan1902@gmail.com>
2023-09-16 14:58:40 +0530

    members API fetch done

--- src/app/components/Profile.tsx
+++ src/app/components/Profile.tsx
@@ -1,15 +1,20 @@
 import React from 'react';
+import Image from 'next/image';
 
 export interface ProfileProps {
-  name: string;
+  username: string;
   bio: string;
+  profile_url:string;
+  role:string;
 }
 
-const Profile: React.FC<ProfileProps> = ({ name, bio }) => {
+const Profile: React.FC<ProfileProps> = ({ username, bio, profile_url, role }) => {
   return (
     <div className="profile">
-      <h3>{name}</h3>
+      <h3>{username}</h3>
       <p>{bio}</p>
+      {/* <Image src={profile_url} alt='profile_url' width={50} height={50}/> */}
+      <span>{role}</span>
     </div>
   );
 };
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,31 +4,31 @@ import Header from './components/Header';
 import Footer from './components/Footer';
 import Hero from './components/Hero';
 import About from './components/About';
-import Community from './components/Community';
+import Members from './components/Members';
 
 const Home: React.FC = () => {
-  const profiles = [
-    // Your profile data here
-    {
-      name: 'kirtan',
-      bio: 'devloper',
-    },
-    {
-      name: 'pratham',
-      bio: 'devloper',
-    },
-    {
-      name: 'ram',
-      bio: 'devloper',
-    }
-  ];
+  // const profiles = [
+  //   // Your profile data here
+  //   {
+  //     name: 'kirtan',
+  //     bio: 'devloper',
+  //   },
+  //   {
+  //     name: 'pratham',
+  //     bio: 'devloper',
+  //   },
+  //   {
+  //     name: 'ram',
+  //     bio: 'devloper',
+  //   }
+  // ];
 
   return (
     <div>
       <Header />
       <Hero />
       <About />
-      <Community profiles={profiles} />
+      <Members />
       <Footer />
     </div>
   );
