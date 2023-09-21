c095dd0e59cfeaa9571b975ea1746a8ac2cab944
kirtan <kirtan1902@gmail.com>
2023-09-21 15:18:32 +0530

    Added Socials

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,10 +1,30 @@
 import React from 'react';
+import Image from 'next/image';
+import github_white from './github-mark-white.png'
+import discord from './discord.png'
+import Link from 'next/link';
 
 const Footer: React.FC = () => {
   return (
     <footer className="py-4">
-      <div className="container mx-auto text-center">
-        <p>&copy; {new Date().getFullYear()} Coders Garden</p>
+      <div className="container mx-auto flex justify-between items-center">
+        <Link href='https://discord.gg/tK5CjMQV'>
+          <Image
+            src={discord}
+            width={30}
+            height={30}
+            alt={'GitHub'}
+          />
+        </Link>
+        <p className='text-gray-400'>&copy; {new Date().getFullYear()} Coders Garden</p>
+        <Link href='https://github.com/coders-garden'>
+          <Image
+            src={github_white}
+            width={30}
+            height={30}
+            alt={'GitHub'}
+          />
+        </Link>
       </div>
     </footer>
   );
