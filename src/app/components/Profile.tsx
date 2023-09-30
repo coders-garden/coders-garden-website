4efa328cf9641156aafc08974e9a01abc3ab927b
Pratham <chudasamapratham142@gmail.com>
2023-09-30 14:49:53 +0530

    more details added

--- src/app/components/Profile.tsx
+++ src/app/components/Profile.tsx
@@ -1,38 +1,62 @@
 import React from "react";
-import Image from "next/image";
 import Link from "next/link";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
-import { Card, CardContent, CardHeader } from "@/components/ui/card";
+import {
+	Card,
+	CardContent,
+	CardDescription,
+	CardHeader,
+} from "@/components/ui/card";
 export interface ProfileProps {
 	username: string;
+	name: string;
 	bio: string;
 	profile_pic_url: string;
 	role: string;
 	github_link: string;
+	repositories: string;
+	following: string;
+	followers: string;
 }
 
 const Profile: React.FC<ProfileProps> = ({
 	username,
+	name,
 	bio,
 	profile_pic_url,
 	role,
 	github_link,
+	repositories,
+	following,
+	followers,
 }) => {
 	return (
 		<Card className="profile">
 			<CardHeader>
-				<Link href={github_link} className="flex gap-3 items-center">
+				<Link
+					href={github_link}
+					className="flex gap-3 items-center w-fit"
+				>
 					<Avatar>
 						<AvatarImage
 							src={profile_pic_url}
-							width={50}
-							height={50}
 							alt={"profile pic of " + username}
 						/>
-						<AvatarFallback>{username[0]}</AvatarFallback>
+						<AvatarFallback>
+							{username[0].toUpperCase()}
+						</AvatarFallback>
 					</Avatar>
 					<span>
-						<h3 className="text-lg font-bold">{username}</h3>
+						<h3 className="text-lg font-bold">
+							{name ?? username}
+						</h3>
+						<CardDescription className="font-bold">
+							Repositories: {repositories}
+						</CardDescription>
+						<CardDescription className="text-xs font-light">
+							<span>Followers: {followers}</span>{" "}
+							<span>Following: {following}</span>
+						</CardDescription>
 					</span>
 				</Link>
 			</CardHeader>
