abd0e6af4413c866a1a55ea290ad9265281bd6e4
Pratham <chudasamapratham142@gmail.com>
2023-10-01 16:39:18 +0530

    made checking after 1 sec

--- src/app/components/Community/CommunityMemberUpdateButton.tsx
+++ src/app/components/Community/CommunityMemberUpdateButton.tsx
@@ -15,7 +15,7 @@ export default function CommunityMemberUpdateButton({
 }) {
 	const { data: session } = useSession();
 	const [updateProfilesLoading, setUpdateProfilesLoading] = useState(false);
-	const [amiadmin, setAmiadmin] = useState<boolean>(false);
+	const [amiadmin, setAmiadmin] = useState(false);
 
 	async function updateCommunity() {
 		setUpdateProfilesLoading(true);
@@ -26,10 +26,20 @@ export default function CommunityMemberUpdateButton({
 	}
 
 	useEffect(() => {
-		setAmiadmin(localStorage.getItem("amiadmin") === "true");
+		if (!session) return;
+		const getAmiadmin = setTimeout(() => {
+			if(localStorage.getItem("amiadmin") === "true"){
+				setAmiadmin(true);
+			}
+		}, 1000);
+		
+		return () => clearTimeout(getAmiadmin);
 	}, [session]);
 
-	if ((session && session.user?.image === profile_pic_url) || (session && amiadmin)){
+	if (
+		(session && session.user?.image === profile_pic_url) ||
+		(session && amiadmin)
+	) {
 		return (
 			<Button onClick={updateCommunity}>
 				<UpdateIcon
